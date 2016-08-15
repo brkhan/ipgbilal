acf534f7770b1dce25815466baed5a233bc8f2de
Bilal Khan <Bilal Khan>
2016-08-15 01:43:04 +0100

    commit

--- build.ps1
+++ build.ps1
@@ -14,14 +14,14 @@ $psakeModule = (Get-ChildItem (".\Packages\psake*\tools\psake.psm1")).FullNam
 Import-Module $psakeModule
 
 properties {
-$solnfile = "./iPgBilal.sln"
-$build_artifacts_dir_loc = "./BuildArtifacts"
+$solnfile = "..\iPgBilal.sln"
+$build_artifacts_dir_loc = "..\BuildArtifacts"
 }
 
 #task default -depends start
 
 #task start {
-Invoke-psake Build/default.ps1  -parameters @{paramsoln_dir ="./iPgBilal.sln"; parambuildartifacts_dir = "./BuildArtifacts"} -framework 4.5.2x64
+Invoke-psake Build/default.ps1  -parameters @{paramsoln_dir ="..\iPgBilal.sln"; parambuildartifacts_dir = "..\BuildArtifacts"} -framework 4.5.2x64
 #}
 
 Write-Host "Build exit code:" $LastExitCode
