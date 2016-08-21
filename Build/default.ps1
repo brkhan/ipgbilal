ab52596db1f6d529a36f018949f480139b224b15
Bilal Khan <Bilal Khan>
2016-08-21 22:37:31 +0100

    create web deploy package as zip

--- Build/default.ps1
+++ Build/default.ps1
@@ -1,6 +1,8 @@
 ﻿properties {
   $soln = $paramsoln_dir
   $build_artifacts_dir = $parambuildartifacts_dir
+  $proj = $paramPublishedWeb
+  $webPackageLoc_dir = $paramWebPackageLoc
 }
 
 
@@ -14,20 +16,34 @@ if (Test-Path $build_artifacts_dir){
 
 }
 
+if (Test-Path  $webPackageLoc_dir){
+  rd   $webPackageLoc_dir -recurse -force | out-null
+
+}
+
 mkdir $build_artifacts_dir
+mkdir  $webPackageLoc_dir 
 
 Write-Host "Cleaning $soln" -ForegroundColor Green
 	Exec { msbuild "$soln" /t:Clean /p:Configuration=Release /v:minimal } 
-
+	
  Write-Host 'Executed Clean!'
 }
 
 task Compile -depends Clean {
-Write-Host "Compiling $soln" -ForegroundColor Green
-Write-Host "artifacts $build_artifacts_dir" -ForegroundColor Green
-Exec {
-msbuild "$soln" /t:Build /p:Configuration=Release /p:OutputPath=$build_artifacts_dir  /v:minimal}
+Write-Host "Compiling  resolve-path $soln" -ForegroundColor Green
+Write-Host "artifacts resolve-path $build_artifacts_dir" -ForegroundColor Green
+Write-Host "web project resolve-path $proj" -ForegroundColor Green
+Write-Host "web package location  resolve-path $webPackageLoc_dir" -ForegroundColor Green
+
+Exec { msbuild "$soln" /t:Build /p:Configuration=Release /p:OutputPath=$build_artifacts_dir  /v:minimal}
  Write-Host 'Executed Compile!'
+
+		if($LASTEXITCODE -ne 0) {
+        throw "Failed to deploy to Release"
+        exit 1
+    }
+
 }
  
 
@@ -36,11 +52,23 @@ task Test -depends Compile, Clean {
    exec {
 	& ..\NUnit-3.4.1\bin\nunit3-console.exe  $build_artifacts_dir\UnitsTests.dll
 	 }
+
+	 Write-Host 'All tests successful  Tests1!' -ForegroundColor Green
  
 }
 
-task Deployment -depends Test {
- Write-Host 'All tests successful  Tests1!' -ForegroundColor Green
+task Deployment -depends Test  {
+	
+ Exec { msbuild "$proj" /t:Package /p:Platform=AnyCPU /p:Configuration=Release /p:PackageLocation="$webPackageLoc_dir" /v:q }
+
+Write-Host 'Executed Package creation!' -ForegroundColor Green
+
+
+	if($LASTEXITCODE -ne 0) {
+        throw "Failed to deploy to Release"
+        exit 1
+    }
+
 
 }
 
--- build.ps1
+++ build.ps1
@@ -16,15 +16,22 @@ Import-Module $psakeModule
 properties {
 $solnfile = "..\iPgBilal.sln"
 $build_artifacts_dir_loc = "..\BuildArtifacts"
+$packageLoc = "..\Build\PublishedWebsites"
+$webProject = "..\iPagooBilal\iPgBilal.csproj"
 }
 
 #task default -depends start
 
 #task start {
-Invoke-psake Build/default.ps1  -parameters @{paramsoln_dir ="..\iPgBilal.sln"; parambuildartifacts_dir = "..\BuildArtifacts"} -framework 4.5.2x64
+#Invoke-psake Build/default.ps1  -parameters @{paramsoln_dir ="$solnfile"; parambuildartifacts_dir ="$build_artifacts_dir_loc"; paramPublishedWeb = "$webProject"; paramWebPackageLoc="$packageLoc"} -framework 4.5.2x64
+#}
+
+#task start {
+Invoke-psake Build/default.ps1  -parameters @{paramsoln_dir ="..\iPgBilal.sln"; parambuildartifacts_dir = "..\BuildArtifacts"; paramPublishedWeb = "..\iPagooBilal\iPgBilal.csproj"; paramWebPackageLoc="..\Build\PublishedWebsites"} -framework 4.5.2x64
 #}
 
 Write-Host "Build exit code:" $LastExitCode
 
 # Propagating the exit code so that builds actually fail when there is a problem
 exit $LastExitCode
+
